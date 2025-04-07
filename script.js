73a261f6de97475e1e78cd9a161ef635813a40d2
Mimi <trish21215@yahoo.com>
2025-04-07 19:17:30 -0400

    finished inventory excercise

---
+++ script.js
@@ -0,0 +1,21 @@
+let groceryList = [];
+
+let newGroceryList = groceryList.push('Apples', 'Milk', 'Bread', 'Bananas', 'Deli Meat');
+
+console.log(groceryList.length);
+
+groceryList[1] = null;
+groceryList[4] = null;
+
+console.log(groceryList);
+
+groceryList[5] = ''
+groceryList[6] = ''
+
+console.log(groceryList);
+
+groceryList.splice(5, 1, 'Spices');
+groceryList.splice(6, 1, 'Juice');
+
+console.log(groceryList);
+console.log(groceryList.length);
